cfb4625d7c2dee58c6466ff3d20ff2a37c00f10e
Duan-Espinoza <75913012+Duan-Espinoza@users.noreply.github.com>
2025-06-16 23:25:00 -0600

    backend

--- ccss-salud-app-backend/src/controllers/index.js
+++
@@ -1,9 +0,0 @@
-import UserController from './UserController';
-import AuthController from './AuthController';
-import HealthRecordController from './HealthRecordController';
-
-export {
-    UserController,
-    AuthController,
-    HealthRecordController
-};
